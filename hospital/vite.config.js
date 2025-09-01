8b63e86196b6eb66298b39d0af68f50dcaf44519
Sharafath_V <142240872+sharafath07@users.noreply.github.com>
2025-09-01 15:57:03 +0000

    finished frontend

--- hospital/vite.config.js
+++ hospital/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MyCare-Hosital-web", // <-- EXACT repo name with slashes
+  base: "/MyCare-Hospital-web", // <-- EXACT repo name with slashes
 })
